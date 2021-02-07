f8c73c0d3be0ed0169a197c30c7890dda6b9593b
Daria Fediashova <d.fedyashova@bk.ru>
2021-02-07 19:58:12 +0300

    Initial Commit

---
+++ Trekr/TrekrApp.swift
@@ -0,0 +1,17 @@
+//
+//  TrekrApp.swift
+//  Trekr
+//
+//  Created by Дарья Федяшова on 07.02.2021.
+//
+
+import SwiftUI
+
+@main
+struct TrekrApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
